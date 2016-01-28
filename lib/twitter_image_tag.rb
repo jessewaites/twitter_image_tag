8e2f8ec62802d3f0d172a585e91b6b4fde40249e
jessewaites <jesse.waites@gmail.com>
2016-01-28 16:42:56 -0500

    ugh this is getting old

--- lib/twitter_image_tag.rb
+++ lib/twitter_image_tag.rb
@@ -2,6 +2,6 @@ require "twitter_image_tag/version"
 
 module TwitterImageTag
   def self.show_me(username, size)
-    '<img src="https://twitter.com/#{username}/profile_image?size=#{size}" alt="twitter avatar" />'.html_safe
+    <img src="https://twitter.com/#{username}/profile_image?size=#{size}" alt="twitter avatar" />
   end
 end
--- lib/twitter_image_tag/version.rb
+++ lib/twitter_image_tag/version.rb
@@ -1,3 +1,3 @@
 module TwitterImageTag
-  VERSION = "1.9.1"
+  VERSION = "1.9.2"
 end
