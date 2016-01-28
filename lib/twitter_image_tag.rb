ad4139195a44238f79ce875390b3c6c685843eb9
jessewaites <jesse.waites@gmail.com>
2016-01-28 16:38:55 -0500

    version bump 1.9.1

--- lib/twitter_image_tag.rb
+++ lib/twitter_image_tag.rb
@@ -1,9 +1,7 @@
 require "twitter_image_tag/version"
 
-require 'action_view'
 module TwitterImageTag
-  include ActionView::Helpers::AssetTagHelper
   def self.show_me(username, size)
-    image_tag("https://twitter.com/#{username}/profile_image?size=#{size}")
+    '<img src="https://twitter.com/#{username}/profile_image?size=#{size}" alt="twitter avatar" />'.html_safe
   end
 end
--- lib/twitter_image_tag/version.rb
+++ lib/twitter_image_tag/version.rb
@@ -1,3 +1,3 @@
 module TwitterImageTag
-  VERSION = "1.9"
+  VERSION = "1.9.1"
 end
